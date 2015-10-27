28bc3e132a10e12ec595f02a7dbe6a55ad32c7c4
Nathanael Olander <aeves.nate@gmail.com>
2015-10-27 00:55:28 -0400

    Removed changes from #11 fix

--- static/chatstatus.js
+++ static/chatstatus.js
@@ -151,20 +151,6 @@
                 .append($typing)
                 .data('typing', $typing);
             users[userId] = $user;
-
-            // Create the allUsers variable as an array of all present users
-            var allUsers = CHAT.RoomUsers.allPresent().toArray();
-
-            for (i = 0; i < allUsers.length; i++) {
-                // Iterate through the list of present users and if it's equal
-                // to the user's ID we are updating then ...
-                if (allUsers[i].id == userId) {
-                    // ... set the user's element's title attribute equal to
-                    // the user's username
-                    $user.attr('title', allUsers[i].name);
-                }
-            }
-
         }
         return $user;
     }
